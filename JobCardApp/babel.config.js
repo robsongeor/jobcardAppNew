4998818f48113ad0e6b4aa750a00ffe33f1095a0
robsongeor <robsongeorge@hotmail.com>
2025-06-12 22:03:13 +1200

    chore: setup web app - migration to expo from here

--- JobCardApp/babel.config.js
+++ JobCardApp/babel.config.js
@@ -1,3 +1,13 @@
+// babel.config.js
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  overrides: [
+    {
+      test: /\.(js|jsx|ts|tsx)$/,
+      plugins: [
+        ['@babel/plugin-transform-react-jsx-self', false],
+        ['@babel/plugin-transform-react-jsx-source', false],
+      ],
+    },
+  ],
 };
