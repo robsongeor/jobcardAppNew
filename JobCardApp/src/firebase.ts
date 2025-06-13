54bf50b271a299aaf8839878cdeba15787059e73
robsongeor <robsongeorge@hotmail.com>
2025-06-13 23:20:20 +1200

    firebase

---
+++ JobCardApp/src/firebase.ts
@@ -0,0 +1,5 @@
+// src/firebase.ts
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+export { auth, firestore };
---
+++ JobCardApp/src/screens/auth/SignUpScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function SignUpScreen() {
+
+    return (
+        <View>
+            <Text> Signup </Text>
+        </View>
+    )
+}
