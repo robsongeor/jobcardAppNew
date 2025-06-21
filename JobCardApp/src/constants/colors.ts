566adba8c420cde9ca2d6ee58b810d7948d659c6
robsongeor <robsongeorge@hotmail.com>
2025-06-21 19:28:20 +1200

    feat: add color contants

---
+++ JobCardApp/src/constants/colors.ts
@@ -0,0 +1,12 @@
+// colors.ts
+const COLORS = {
+    primary: '#007AFF',
+    success: '#4BB543',
+    greyed: '#CCC',
+    greyedText: '#AAA',
+    danger: '#D7263D',
+    background: '#fff',
+    // ... add more as needed
+};
+
+export default COLORS;
