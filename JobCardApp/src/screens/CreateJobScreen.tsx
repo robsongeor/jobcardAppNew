5e89eac17e1139aacdfb7252a05949c7d10e5310
robsongeor <robsongeorge@hotmail.com>
2025-06-23 12:19:13 +1200

    refactor: abstract job form to seperate file

--- JobCardApp/src/screens/CreateJobScreen.tsx
+++ JobCardApp/src/screens/CreateJobScreen.tsx
@@ -1,8 +1,8 @@
-import { Text, View } from "react-native";
-import SmallTextInput from "../components/form/FormInputs/SmallTextInput";
+import { View } from "react-native";
 import { useState } from "react";
 import { Job } from "../types/types";
 import { getStoredUserField } from "../storage/storage";
+import CreateJobForm from "./CreateJobScreen/createJobForm";
 
 export default function CreateJobScreen() {
     const userID = getStoredUserField("uid");
@@ -33,54 +33,9 @@ export default function CreateJobScreen() {
 
     return (
         <View>
-            <SmallTextInput
-                label="Job Number"
-                value={jobInfo.job}
-                onChangeText={(value) => setJobInfo({ ...jobInfo, job: value })}
-            />
-            <SmallTextInput
-                label="Fleet Number"
-                value={jobInfo.fleet}
-                onChangeText={(value) => setJobInfo({ ...jobInfo, fleet: value })}
-            />
-            <SmallTextInput
-                label="Customer Name"
-                value={jobInfo.customerName}
-                onChangeText={(value) => setJobInfo({ ...jobInfo, customerName: value })}
-            />
-            <SmallTextInput
-                label="Make"
-                value={jobInfo.machine.make}
-                onChangeText={(value) =>
-                    setJobInfo({ ...jobInfo, machine: { ...jobInfo.machine, make: value } })
-                }
-            />
-            <SmallTextInput
-                label="Model"
-                value={jobInfo.machine.model}
-                onChangeText={(value) =>
-                    setJobInfo({ ...jobInfo, machine: { ...jobInfo.machine, model: value } })
-                }
-            />
-            <SmallTextInput
-                label="Serial"
-                value={jobInfo.machine.serialNumber}
-                onChangeText={(value) =>
-                    setJobInfo({
-                        ...jobInfo,
-                        machine: { ...jobInfo.machine, serialNumber: value },
-                    })
-                }
-            />
-            <SmallTextInput
-                label="Description"
-                value={jobInfo.description}
-                onChangeText={(value) => setJobInfo({ ...jobInfo, description: value })}
-            />
-            <SmallTextInput
-                label="Site (optional)"
-                value={jobInfo.site}
-                onChangeText={(value) => setJobInfo({ ...jobInfo, site: value })}
+            <CreateJobForm
+                jobInfo={jobInfo}
+                setJobInfo={setJobInfo}
             />
         </View>
 
---
+++ JobCardApp/src/screens/CreateJobScreen/createJobForm.tsx
@@ -0,0 +1,64 @@
+import SmallTextInput from "../../components/form/FormInputs/SmallTextInput";
+import { Job } from "../../types/types";
+
+type createJobFormProps = {
+    jobInfo: Job;
+    setJobInfo: React.Dispatch<React.SetStateAction<Job>>;
+}
+
+export default function CreateJobForm({ jobInfo, setJobInfo }: createJobFormProps) {
+
+    return (
+        <>
+            <SmallTextInput
+                label="Job Number"
+                value={jobInfo.job}
+                onChangeText={(value) => setJobInfo({ ...jobInfo, job: value })}
+            />
+            <SmallTextInput
+                label="Fleet Number"
+                value={jobInfo.fleet}
+                onChangeText={(value) => setJobInfo({ ...jobInfo, fleet: value })}
+            />
+            <SmallTextInput
+                label="Customer Name"
+                value={jobInfo.customerName}
+                onChangeText={(value) => setJobInfo({ ...jobInfo, customerName: value })}
+            />
+            <SmallTextInput
+                label="Make"
+                value={jobInfo.machine.make}
+                onChangeText={(value) =>
+                    setJobInfo({ ...jobInfo, machine: { ...jobInfo.machine, make: value } })
+                }
+            />
+            <SmallTextInput
+                label="Model"
+                value={jobInfo.machine.model}
+                onChangeText={(value) =>
+                    setJobInfo({ ...jobInfo, machine: { ...jobInfo.machine, model: value } })
+                }
+            />
+            <SmallTextInput
+                label="Serial"
+                value={jobInfo.machine.serialNumber}
+                onChangeText={(value) =>
+                    setJobInfo({
+                        ...jobInfo,
+                        machine: { ...jobInfo.machine, serialNumber: value },
+                    })
+                }
+            />
+            <SmallTextInput
+                label="Description"
+                value={jobInfo.description}
+                onChangeText={(value) => setJobInfo({ ...jobInfo, description: value })}
+            />
+            <SmallTextInput
+                label="Site (optional)"
+                value={jobInfo.site}
+                onChangeText={(value) => setJobInfo({ ...jobInfo, site: value })}
+            />
+        </>
+    )
+}
