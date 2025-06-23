ffca228922c59d2d763b2eb19f85145d960ad62e
robsongeor <robsongeorge@hotmail.com>
2025-06-23 12:28:58 +1200

    feat: basic form validation

--- JobCardApp/src/screens/CreateJobScreen.tsx
+++ JobCardApp/src/screens/CreateJobScreen.tsx
@@ -1,11 +1,14 @@
 import { View } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Job } from "../types/types";
 import { getStoredUserField } from "../storage/storage";
 import CreateJobForm from "./CreateJobScreen/createJobForm";
+import BottomRightButton from "../components/form/Buttons/BottomRightButton";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function CreateJobScreen() {
     const userID = getStoredUserField("uid");
+    const [submitDisabled, setSubmitDisable] = useState(true)
 
     const [jobInfo, setJobInfo] = useState<Job>({
         id: "",
@@ -30,14 +33,34 @@ export default function CreateJobScreen() {
         siteContactPhone: "",
     });
 
+    function isJobInfoValid(jobInfo: Job): boolean {
+        return (
+            jobInfo.fleet.trim().length > 0 &&
+            jobInfo.job.trim().length > 0 &&
+            jobInfo.customerName.trim().length > 0 &&
+            jobInfo.machine.make.trim().length > 0 &&
+            jobInfo.machine.model.trim().length > 0
+        );
+    }
+
+    useEffect(() => {
+        setSubmitDisable(!isJobInfoValid(jobInfo));
+    }, [jobInfo]);
+
 
     return (
-        <View>
+        <SafeAreaView style={{ flex: 1 }}>
             <CreateJobForm
                 jobInfo={jobInfo}
                 setJobInfo={setJobInfo}
             />
-        </View>
+            <BottomRightButton
+                label="Assign"
+                disabled={submitDisabled}
+                onPress={() => console.log("Assigned")}
+                icon="user-plus"
+            />
+        </SafeAreaView>
 
     )
 }
