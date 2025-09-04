2a1d2b313d8d0e5adb27815b13076410b069c05e
robsongeor <robsongeorge@hotmail.com>
2025-09-05 09:03:52 +1200

    chore: fix android nav overlap bug

--- JobCardApp/index.js
+++ JobCardApp/index.js
@@ -1,18 +1,19 @@
-/**
- * @format
- */
-
 import 'react-native-get-random-values';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const Root = () => (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-        <App />
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }} edges={['bottom', 'left', 'right']}>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <App />
+            </GestureHandlerRootView>
+        </SafeAreaView>
+    </SafeAreaProvider>
 );
 
 AppRegistry.registerComponent(appName, () => Root);
